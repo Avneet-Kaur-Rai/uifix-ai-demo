9126db8aee8a2a7b56f0d4d567bab619939d3ad4
Avneet Kaur Rai <avni.rai013@gmail.com>
2026-01-15 11:33:28 +0530

    accessibility fix

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -9,13 +9,17 @@ const Button = ({
   disabled = false,
   fullWidth = false,
   className = '',
+  ariaLabel, // accessibility: allow aria-label for icon-only buttons
+  ...rest
 }) => {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
+      aria-label={ariaLabel}
       className={`btn btn-${variant} btn-${size} ${fullWidth ? 'btn-full-width' : ''} ${className}`}
+      {...rest}
     >
       {children}
     </button>
